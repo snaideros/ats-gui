c9376f1df2f469742be2d28191b2e5bff06be1de
Sébastien Snaider <sebastien.snaidero@gmail.com>
2024-04-29 05:47:41 +0200

    remove unused pinia

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -6,12 +6,10 @@
 
 // Plugins
 import vuetify from './vuetify'
-import pinia from '@/stores'
 import router from '@/router'
 
 export function registerPlugins (app) {
   app
     .use(vuetify)
     .use(router)
-    .use(pinia)
 }
