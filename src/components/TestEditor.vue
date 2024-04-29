6965d8e1a2623b5233c5a75ba0b9c05d376ae0c7
Sébastien Snaider <sebastien.snaidero@gmail.com>
2024-04-29 12:11:13 +0200

    add save button to breadcrumbs

--- src/components/TestEditor.vue
+++ src/components/TestEditor.vue
@@ -1,13 +1,16 @@
 <template>
     <v-container class="bg-cyan fill-height pa-0" fluid>
         <v-row class="flex-column bg-pink fill-height" no-gutters>
-            <v-col class="bg-deep-purple flex-shrink-1 flex-grow-0">
-                <v-breadcrumbs class="bg-yellow py-1" density="compact" :items="['Foo', 'Bar', 'Fizz']">
-                    <template v-slot:prepend>
-                        <v-icon icon="mdi-home" size="small"></v-icon>
-                        <v-breadcrumbs-divider />
-                    </template>
-                </v-breadcrumbs>
+            <v-col class="bg-secondary flex-shrink-1 flex-grow-0">
+                <div class="d-flex">
+                    <v-breadcrumbs class="py-1 flex-fill" density="compact" :items="['Foo', 'Bar', 'Fizz']">
+                        <template v-slot:prepend>
+                            <v-icon icon="mdi-home" size="small"></v-icon>
+                            <v-breadcrumbs-divider />
+                        </template>
+                    </v-breadcrumbs>
+                    <v-btn size="x-small" tile variant="text" icon="mdi-content-save" title="Save (Ctrl+S)"></v-btn>
+                </div>
             </v-col>
             <v-col class="bg-indigo">
                 <v-row class="bg-purple fill-height" no-gutters>
