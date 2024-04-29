d9193c663027b53bb3d44ad317bceb3fc79436e7
Sébastien Snaider <sebastien.snaidero@gmail.com>
2024-04-29 05:52:52 +0200

    simplification

--- src/layouts/home.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <v-app>
-    <h1>Home</h1>
-  </v-app>
-</template>
-
-<script setup>
-  //
-</script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,13 +6,13 @@
           </v-col>
           <v-col class="bg-indigo">
               <v-row class="bg-purple fill-height" no-gutters>
-                  <v-col cols="2" class="bg-red" style="position:relative;">
-                      <div class="bg-green pa-1" style="position:absolute;top:0;left:0;right:0;bottom:0;overflow-y: auto;">
+                  <v-col cols="2" class="bg-red rel">
+                      <div class="bg-green pa-1 abs" style="overflow-y: auto;">
                           <TestExplorer/>
                       </div>
                   </v-col>
-                  <v-col cols="10" class="bg-blue" style="position:relative;">
-                      <div class="bg-lime pa-1" style="position:absolute;top:0;left:0;right:0;bottom:0;">
+                  <v-col cols="10" class="bg-blue rel">
+                      <div class="bg-lime pa-1 abs">
                           <IDE/>
                       </div>
                   </v-col>
@@ -25,6 +25,20 @@
 <script setup>
 //
 </script>
+
+<style>
+.rel {
+  position: relative;
+}
+
+.abs {
+  position:absolute;
+  top:0;
+  left:0;
+  right:0;
+  bottom:0;
+}
+</style>
 
 <route>
 {
